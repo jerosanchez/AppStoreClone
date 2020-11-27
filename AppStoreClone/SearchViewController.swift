ab3ecb035a580583491911e3dbce904bd23f1828
Jero Sanchez <jero.sanchez@icloud.com>
2020-11-27 11:18:26 +0100

    Create basic dummy layout for Search tab

--- AppStoreClone/SearchViewController.swift
+++ AppStoreClone/SearchViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SearchViewController: UICollectionViewController {
+final class SearchViewController: UICollectionViewController {
     
     convenience init() {
         self.init(collectionViewLayout: UICollectionViewFlowLayout())
@@ -17,5 +17,23 @@ class SearchViewController: UICollectionViewController {
         super.viewDidLoad()
         
         collectionView.backgroundColor = .white
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+}
+
+extension SearchViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 250)
     }
 }
