500bf32d43adef1cc20cb4a44000b7f8fd140de6
Jero Sanchez <jero.sanchez@icloud.com>
2020-11-29 16:42:29 +0100

    Fetch app details on loading App details screen

--- AppStoreClone/UI/AppDetails/AppDetailsViewController.swift
+++ AppStoreClone/UI/AppDetails/AppDetailsViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 final class AppDetailsViewController: UIViewController {
     
     private let appId: String
+    private let appDetailsService = AppDetailsService()
     
     init(appId: String) {
         self.appId = appId
@@ -23,7 +24,25 @@ final class AppDetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        fetchData()
+        
         view.backgroundColor = .white
         print("App id: \(appId)")
+    }
+    
+    // MARK: - Helpers
+    
+    private func fetchData() {
+        appDetailsService.load(appId: appId) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case let .success(appDetails):
+                print(appDetails)
+                
+            case let .failure(error):
+                print("Load failed: \(error)")
+            }
+        }
     }
 }
