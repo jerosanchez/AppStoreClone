883138487386add5a770ef13415d6f441dcd83ea
Jero Sanchez <jero.sanchez@icloud.com>
2020-11-30 19:21:19 +0100

    Layout dummy items in the reviews cell

---
+++ AppStoreClone/UI/AppDetails/Reviews/AppDetailsReviewCardCell.swift
@@ -0,0 +1,55 @@
+//
+//  AppDetailsReviewCardCell.swift
+//  AppStoreClone
+//
+//  Created by Jero Sanchez on 30/11/20.
+//
+
+import UIKit
+
+final class AppDetaulsReviewCardCell: UICollectionViewCell {
+    
+    static var cellId: String { return AppDetaulsReviewCardCell.description() }
+
+    private let titleLabel = UILabel(text: "Review title", font: .systemFont(ofSize: 16, weight: .bold))
+    
+    private let authorLabel: UILabel = {
+        let label = UILabel(text: "Author", font: .systemFont(ofSize: 16, weight: .regular))
+        label.textColor = .lightGray
+        return label
+    }()
+    
+    private let starsLabel = UILabel(text: "Stars", font: .systemFont(ofSize: 14, weight: .regular))
+    
+    private let bodyLabel = UILabel(text: "Review body\nReview body\nReview body", font: .systemFont(ofSize: 14, weight: .regular), numberOfLines: 0)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = UIColor(red: 240/255, green: 239/255, blue: 248/255, alpha: 1)
+        layer.cornerRadius = 16
+        clipsToBounds = true
+
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helpers
+    
+    private func setupLayout() {
+        let headerStackView = UIStackView(arrangedSubviews: [
+            titleLabel, UIView(), authorLabel
+        ])
+        headerStackView.spacing = 8
+        
+        let stackView = VerticalStackView(arrangedSubviews: [
+            headerStackView, starsLabel, bodyLabel, UIView()
+        ], spacing: 8)
+        
+        addSubview(stackView)
+        stackView.fillSuperview(padding: .init(top: 16, left: 16, bottom: 16, right: 16))
+    }
+}
--- AppStoreClone/UI/AppDetails/Reviews/AppDetailsReviewsController.swift
+++ AppStoreClone/UI/AppDetails/Reviews/AppDetailsReviewsController.swift
@@ -20,29 +20,34 @@ final class AppDetailsReviewsController: HSnappingCollectionViewController {
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .blue
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AppDetaulsReviewCardCell.cellId, for: indexPath)
         return cell
     }
 
     // MARK: - Helpers
     
+    private var bottomPadding: CGFloat = 16
+    
     private func setup() {
         collectionView.backgroundColor = .white
 
-        collectionView.contentInset = .init(top: 0, left: 16, bottom: 0, right: 16)
+        collectionView.contentInset = .init(top: 0, left: 16, bottom: bottomPadding, right: 16)
 
         registerCells()
     }
 
     private func registerCells() {
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.register(AppDetaulsReviewCardCell.self, forCellWithReuseIdentifier: AppDetaulsReviewCardCell.description())
     }
 }
 
 extension AppDetailsReviewsController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return .init(width: view.frame.width - 48, height: view.frame.height)
+        return .init(width: view.frame.width - 48, height: view.frame.height - bottomPadding)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 16
     }
 }
